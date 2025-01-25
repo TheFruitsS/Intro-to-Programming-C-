ac796e9e1dce9ff881d507c9a82d0f99f3f8c0ba
Dimitar Dzhevizov <dimitardzhevizov88@gmail.com>
2025-01-25 11:52:56 +0000

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    int myAge; //this is first
-    printf("What is your age?");
-    scanf("%d", &myAge);
-    printf("Nice to meet you,you are %d old!", myAge);
-    return 0;
-}
