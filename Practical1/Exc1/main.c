716726738c9a0449218483865b08e3de64c0c802
Dimitar Dzhevizov <dimitardzhevizov88@gmail.com>
2025-01-25 12:11:02 +0000

    what is your age

---
+++ Practical1/Exc1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int myAge; //this is first
+    printf("What is your age?");
+    scanf("%d", &myAge);
+    printf("Nice to meet you,you are %d old!", myAge);
+    return 0;
+}
